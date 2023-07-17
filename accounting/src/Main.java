4783a5dbe40dd1ecf925bb381cd18ac56b8cb764
Anastasiia Hopchuk <anastasiiahopch@gmail.com>
2023-07-17 10:51:07 +0200

    Finalising the code.

--- accounting/src/Main.java
+++ accounting/src/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-//import java.util.Arrays;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -12,6 +12,7 @@ public class Main {
         String[][][] linesMonthReports = new String[12][][];
         String yearReport;
         String[][][] linesYearReport = new String[1][][];
+        boolean isYear = false, isMonth = false;
 
         int command;
          do {
@@ -26,50 +27,49 @@ public class Main {
 
                     for (int i = 0; i < 12; i++) {
                         linesMonthReports[i] = readReport(monthReports[i]);
-                        /*if (linesMonthReports[i] != null)
-                            for (String[] line : linesMonthReports[i]) {
-                                for (String l : line)
-                                    System.out.println(l);
-                            }*/
-                    }
+                    } isMonth = true;
                 }
                 case 2 -> {
                     yearReport = readFileContentsOrNull("y.2022");
                     linesYearReport[0] = readReport(yearReport);
 
-                    /*if (linesYearReport[0] != null)
-                        for (String[] line : linesYearReport[0]) {
-                            for (String l : line) {
-                                System.out.print(l);
-                            }
-                            System.out.println();
-                        }*/
-
+                    isYear = true;
                 }
                 case 3 -> {
                     float[] sumM = new float[12];
                     float[] sumY = new float[12];
 
-                    for (int i = 0; i < 12; i++) {
-                        if (linesMonthReports[i] != null) {
-                            for (int j = 1; j < linesMonthReports[i].length; j++) {
-                                if (linesMonthReports[i][j] != null) {
-                                    if (linesMonthReports[i][j][1].equals("TRUE")) {
-                                        sumM[i] -= (Float.parseFloat(linesMonthReports[i][j][2]) * Float.parseFloat(linesMonthReports[i][j][3]));
-                                    } else if (linesMonthReports[i][j][1].equals("FALSE"))
-                                        sumM[i] += (Float.parseFloat(linesMonthReports[i][j][2]) * Float.parseFloat(linesMonthReports[i][j][3]));
+                    if (!isMonth) {
+                        System.out.println("You have not read monthly reports");
+                        break;
+                    }
+                    else {
+                        for (int i = 0; i < 12; i++) {
+                            if (linesMonthReports[i] != null) {
+                                for (int j = 1; j < linesMonthReports[i].length; j++) {
+                                    if (linesMonthReports[i][j] != null) {
+                                        if (linesMonthReports[i][j][1].equals("TRUE")) {
+                                            sumM[i] -= (Float.parseFloat(linesMonthReports[i][j][2]) * Float.parseFloat(linesMonthReports[i][j][3]));
+                                        } else if (linesMonthReports[i][j][1].equals("FALSE"))
+                                            sumM[i] += (Float.parseFloat(linesMonthReports[i][j][2]) * Float.parseFloat(linesMonthReports[i][j][3]));
+                                    }
                                 }
                             }
                         }
                     }
-
-                    if (linesYearReport[0] != null) {
-                        for (int i = 1; i < linesYearReport[0].length; i++) {
-                            if (linesYearReport[0][i] != null) {
-                                if (linesYearReport[0][i][2].equals("TRUE\r")) {
-                                    sumY[Integer.parseInt(linesYearReport[0][i][0])-1] -= Float.parseFloat(linesYearReport[0][i][1]);
-                                } else if (linesYearReport[0][i][2].equals("FALSE\r")) {
-                                    sumY[Integer.parseInt(linesYearReport[0][i][0])-1] += Float.parseFloat(linesYearReport[0][i][1]);
+                    if (!isYear) {
+                        System.out.println("You have not read annual report");
+                        break;
+                    }
+                    else {
+                        if (linesYearReport[0] != null) {
+                            for (int i = 1; i < linesYearReport[0].length; i++) {
+                                if (linesYearReport[0][i] != null) {
+                                    if (linesYearReport[0][i][2].equals("TRUE\r")) {
+                                        sumY[Integer.parseInt(linesYearReport[0][i][0]) - 1] -= Float.parseFloat(linesYearReport[0][i][1]);
+                                    } else if (linesYearReport[0][i][2].equals("FALSE\r")) {
+                                        sumY[Integer.parseInt(linesYearReport[0][i][0]) - 1] += Float.parseFloat(linesYearReport[0][i][1]);
+                                    }
                                 }
                             }
                         }
@@ -79,11 +79,77 @@ public class Main {
                     else {
                         System.out.println("Something is incorrect.");
                         for (int i = 0; i < 12; i++) {
-                            if (sumM[i] != sumY[i]) System.out.println("Month " + (i+1) + ":\n Month report: " + sumM[i]+ "\n Year report: " + sumY[i]);
+                            if (sumM[i] != sumY[i]) System.out.println("Month " + (i+1) + ":\n\tMonth report: " + sumM[i]+ "\n\tYear report: " + sumY[i]);
                         }
                     }
                 }
+                case 4 ->
+                {
+                    float maxT = 0, maxF = 0;
+                    String nameT = "", nameF = "";
+                    if (!isMonth) {
+                        System.out.println("You have not read monthly reports");
+                        break;
+                    } else {
+                        for (int i = 0; i < 12; i++) {
+                            if (linesMonthReports[i] != null) {
+                                for (int j = 1; j < linesMonthReports[i].length; j++) {
+                                    if (linesMonthReports[i][j] != null) {
+                                        if (linesMonthReports[i][j][1].equals("TRUE")) {
+                                            if ((Float.parseFloat(linesMonthReports[i][j][2]) * Float.parseFloat(linesMonthReports[i][j][3])) > maxT) {
+                                                maxT = (Float.parseFloat(linesMonthReports[i][j][2]) * Float.parseFloat(linesMonthReports[i][j][3]));
+                                                nameT = linesMonthReports[i][j][0];
+                                            }
+                                        } else if (linesMonthReports[i][j][1].equals("FALSE"))
+                                            if ((Float.parseFloat(linesMonthReports[i][j][2]) * Float.parseFloat(linesMonthReports[i][j][3])) > maxF) {
+                                                maxF = (Float.parseFloat(linesMonthReports[i][j][2]) * Float.parseFloat(linesMonthReports[i][j][3]));
+                                                nameF = linesMonthReports[i][j][0];
+                                            }
+                                    }
+                                }
+                            }
+                            System.out.println("Month " + (i+1) + ":");
+                            System.out.println("\tMax income: " + nameF + " - " + maxF);
+                            System.out.println("\tMax expense: " + nameT + " - " + maxT);
+                            maxF = 0; maxT = 0;
+                            nameF = ""; nameT = "";
+                        }
+                    }
+                }
+                case 5 ->
+                {
+                    float avgExpense = 0, avgIncome = 0;
+                    float[] income = new float[12];
+                    Arrays.fill(income, 0);
+
+                    if (!isYear) {
+                        System.out.println("You have not read annual report");
+                        break;
+                    } else {
+                            if (linesYearReport[0] != null) {
+                                for (int i = 1; i < linesYearReport[0].length; i++) {
+                                    if (linesYearReport[0][i] != null) {
+                                        if (linesYearReport[0][i][2].equals("TRUE\r")) {
+                                            income[Integer.parseInt(linesYearReport[0][i][0]) - 1] -= Float.parseFloat(linesYearReport[0][i][1]);
+                                            avgExpense += Float.parseFloat(linesYearReport[0][i][1]);
+                                        } else if (linesYearReport[0][i][2].equals("FALSE\r")) {
+                                            income[Integer.parseInt(linesYearReport[0][i][0]) - 1] += Float.parseFloat(linesYearReport[0][i][1]);
+                                            avgIncome += Float.parseFloat(linesYearReport[0][i][1]);
+                                        }
+                                    }
+                                }
+                            }
+
+                        for (int i = 0; i < 12; i++)
+                        {
+                            System.out.println("Month " + (i+1) + " income: " + income[i]);
+                        }
+                        System.out.println("\nAverage income: " + avgIncome/12);
+                        System.out.println("Average expanse: " + avgExpense/12);
+                    }
+                }
                 case 0 -> System.out.println("Exit");
+                default -> System.out.println("Sorry, this command is not available");
             }
         } while (command!=0);
     }
@@ -91,7 +157,7 @@ public class Main {
     private static void printMenu() {
         System.out.println("Menu:");
         System.out.println("1 - Read all monthly reports");
-        System.out.println("2 - Read yearly report");
+        System.out.println("2 - Read annual report");
         System.out.println("3 - Compare reports");
         System.out.println("4 - Show monthly reports info");
         System.out.println("5 - Show yearly report info");
@@ -108,6 +174,7 @@ public class Main {
             return null;
         }
     }
+
     private static String[][] readReport(String report) {
         if (report == null) return null;
         String[] arr = report.split("\\n");
