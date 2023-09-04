5f90f3c3bc1fed2f9b650bddfb0718582c98d9f7
Anatasiia Hopchuk <132471858+nastasi3a@users.noreply.github.com>
2023-09-04 15:16:06 +0200

    Update Main.java

--- taskTracker/src/Main.java
+++ taskTracker/src/Main.java
@@ -6,7 +6,6 @@ public class Main {
         taskManager.createEpic("Epic2", "This epic has 1 subtask"); //id: 1
         taskManager.createTask("Task1", "This is task nr 1"); //id: 2
         taskManager.createTask("Task2", "This is task nr 2"); //id: 3
-<<<<<<< HEAD
         taskManager.createSubtask(0, "Subtask1", "This is first subtask in epic 1"); //id: 4
         taskManager.createSubtask(0, "Subtask2", "This is second subtask in epic 1"); //id: 5
         taskManager.createSubtask(1, "Subtask1", "This is first subtask in epic 2"); //id: 6
@@ -14,20 +13,10 @@ public class Main {
         System.out.println(taskManager.getEpic(0));
         System.out.println(taskManager.getEpic(1));
         System.out.println(taskManager.getSubtask(4));
-=======
-        taskManager.createSubtaskInEpic(0, "Subtask1", "This is first subtask in epic 1"); //id: 4
-        taskManager.createSubtaskInEpic(0, "Subtask2", "This is second subtask in epic 1"); //id: 5
-        taskManager.createSubtaskInEpic(1, "Subtask1", "This is first subtask in epic 2"); //id: 6
-        taskManager.changeSubtaskStatus(0, 5, "DONE");
-        taskManager.getEpic(0);
-        taskManager.getEpic(1);
-        taskManager.getSubtask(4);
->>>>>>> c63029756d1dc9705d150e5d03ccc0a47a1f0c95
         System.out.println(taskManager.getHistory());
         taskManager.changeSubtaskStatus(1, 6, "IN_PROGRESS");
         taskManager.removeById(4);
         taskManager.removeEpic(0);
-<<<<<<< HEAD
         System.out.println(taskManager.getTask(2));
         System.out.println(taskManager.getHistory());
         System.out.println(taskManager.getEpic(1));
@@ -37,17 +26,6 @@ public class Main {
         System.out.println(taskManager.getSubtask(4));
         System.out.println(taskManager.getHistory());
         System.out.println(taskManager.getTask(3));
-=======
-        taskManager.getTask(2);
-        System.out.println(taskManager.getHistory());
-        taskManager.getEpic(1);
-        System.out.println(taskManager.getHistory());
-        taskManager.getEpic(0);
-        taskManager.getTask(3);
-        taskManager.getSubtask(4);
-        System.out.println(taskManager.getHistory());
-        taskManager.getTask(3);
->>>>>>> c63029756d1dc9705d150e5d03ccc0a47a1f0c95
         System.out.println(taskManager.getHistory());
     }
 }
